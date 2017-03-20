1de7a69ae8e07e6bffce87936066ad1669272329
kitnu <kitnumaster@gmail.com>
2017-03-20 14:06:29 +0700

    x
    x

--- index.php
+++ index.php
@@ -15,7 +15,7 @@ if (!is_null($events['events']))
         if ($event['type'] == 'message' && $event['message']['type'] == 'text')
         {
             // Get text sent
-            $text = "ควย";
+            $text = $event['message']['text'];
             // Get replyToken
             $replyToken = $event['replyToken'];
 
